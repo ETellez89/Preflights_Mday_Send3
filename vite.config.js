14f9cc09d1a26a73461b2a37eb70dd7428e02085
ETellez89 <etellez@groupon.com>
2026-03-12 11:55:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     base: '/Preflights_Mday26_Send3/',
-  plugins: [react()],
+  plugins: [react()], 
 })
